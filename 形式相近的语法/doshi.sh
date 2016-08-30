a6a7540cd23a03bc9cd0d7dd49dd43ffc51280bb
Liming Yu <Liming Yu>
2016-08-30 23:02:11 +0800

    .

---
+++ 形式相近的语法/doshi.sh
@@ -0,0 +1,22 @@
+file=$1
+if [ "$file" = "" ]; then
+    echo "Error: need param"
+    exit
+fi
+lines=$(wc -l $file | awk '{print $1}')
+read -p "y/n:" yn
+if [ "$yn" = "y" ]; then
+    for((i=2;i<=$lines;i++)); do
+        cat $file | head -n $i | tail -n 1 | awk '{print $3}'
+        read -s
+        cat $file | head -n $i | tail -n 1 | awk '{print $4}'
+    done
+else
+    while true; do
+        r=$(expr $RANDOM % $lines)
+        cat $file | head -n $r | tail -n 1 | awk '{print $3}'
+        read -s
+        cat $file | head -n $r | tail -n 1 | awk '{print $4}'
+    done
+fi
+
